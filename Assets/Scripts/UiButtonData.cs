3fc82ad012a6407d8f9eeff25981976a0f5168d9
Gustavo Menezes <gustavo.e.menezes@outlook.com>
2023-07-27 17:10:45 -0300

    preço das armas, UIBotton

--- Assets/Scripts/UiButtonData.cs
+++ Assets/Scripts/UiButtonData.cs
@@ -8,29 +8,43 @@ public class UiButtonData : MonoBehaviour
     public BuildTool buildTool;
     public UIManager uiManager;
     public GameManager gameManager;
+    public int[] towerCosts;
     public int money;
 
     public void Start()
     {
         uiManager = FindObjectOfType<UIManager>();
         gameManager = FindObjectOfType<GameManager>();
+        money = gameManager.playerMoney;
     }
 
     public void OnButtonClick(int towerIndex)
     {
         if (towerIndex >= 0 && towerIndex < towers.Length)
         {
-            buildTool.SetCurrentBuildingData(towers[towerIndex]);
-            gameManager.LoseMoney(money);
-            buildTool.ActivatePreviewAndPrefab();
-            buildTool.towerActivated = true;
-            uiManager.BuildPanel.SetActive(false);
-            uiManager.SetMouseCursorState(false);
+            int towerCost = towerCosts[towerIndex];
 
+            // Verifique se o jogador tem dinheiro suficiente
+            if (money >= towerCost)
+            {
+                buildTool.SetCurrentBuildingData(towers[towerIndex]);
+                gameManager.LoseMoney(towerCost);
+                buildTool.ActivatePreviewAndPrefab();
+                buildTool.towerActivated = true;
+                uiManager.BuildPanel.SetActive(false);
+                uiManager.SetMouseCursorState(false);
+
+                // Atualize o valor do dinheiro (money) após a compra da torre
+                money -= towerCost;
+            }
+            else
+            {
+                Debug.Log("Dinheiro insuficiente para construir esta torre.");
+            }
         }
         else
         {
-            Debug.LogError("Invalid tower index.");
+            Debug.LogError("Índice de torre inválido.");
         }
     }
 
