551e9d7991f87ae975c4bf64ea05d775abbf11fb
Gustavo Menezes <gustavo.e.menezes@outlook.com>
2023-07-28 17:09:59 -0300

    MainMenu + cenário modelo

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Nome da cena que será carregada quando o botão "Play" for pressionado
+    public string sceneToLoad = "Game1";
+
+    // Método chamado quando o botão "Play" é pressionado
+    public void OnPlayButtonPressed()
+    {
+        // Carrega a cena do jogo
+        SceneManager.LoadScene(sceneToLoad);
+        Debug.Log("play");
+    }
+
+    // Método chamado quando o botão "Quit" é pressionado
+    public void OnQuitButtonPressed()
+    {
+        // Sai do jogo (funciona apenas na build final, não no editor)
+        Application.Quit();
+    }
+
+    public void menu()
+    {
+        Debug.Log("PLAY");
+    }
+}
