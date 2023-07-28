4d6e4d0862d8c71cade59f0e9f60375829c450e9
Gustavo Menezes <gustavo.e.menezes@outlook.com>
2023-07-28 16:19:00 -0300

    Sistema de menu de pause

--- Assets/Scripts/GameManagerUI.cs
+++ Assets/Scripts/GameManagerUI.cs
@@ -1,42 +1,88 @@
 ﻿using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-    public class GameManagerUI : MonoBehaviour
+
+public class GameManagerUI : MonoBehaviour
+{
+    public Slider castleHealthSlider;
+    public TextMeshProUGUI moneyText;
+    public GameObject menuObject;
+
+    private GameManager gameManager;
+
+    void Start()
     {
-        public Slider castleHealthSlider;
-        public TextMeshProUGUI moneyText;
+        gameManager = FindObjectOfType<GameManager>();
+        UpdateUI();
+    }
 
-        private GameManager gameManager;
+    void Update()
+    {
+        // Verifica constantemente a vida do castelo para atualizar o Slider
+        UpdateUI();
 
-        void Start()
+        // Verifica se a tecla "P" foi pressionada
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            gameManager = FindObjectOfType<GameManager>();
-            UpdateUI();
-        }
-
-        void Update()
-        {
-            // Verifica constantemente a vida do castelo para atualizar o Slider
-            UpdateUI();
-        }
-
-        // Método para atualizar a UI do Slider da vida do castelo e do dinheiro do jogador
-        private void UpdateUI()
-        {
-            UpdateCastleHealthUI();
-            UpdateMoneyUI();
-        }
-
-        // Método para atualizar o Slider da vida do castelo
-        private void UpdateCastleHealthUI()
-        {
-            castleHealthSlider.value = (float)gameManager.GetCastleHealth() / gameManager.GetMaxCastleHealth();
-        }
-
-        // Método para atualizar o texto de dinheiro do jogador usando TextMeshProUGUI
-        private void UpdateMoneyUI()
-        {
-            moneyText.text = gameManager.GetPlayerMoney().ToString() + "$";
+            ToggleMenuAndCursor();
         }
     }
-    
+
+    // Método para atualizar a UI do Slider da vida do castelo e do dinheiro do jogador
+    private void UpdateUI()
+    {
+        UpdateCastleHealthUI();
+        UpdateMoneyUI();
+    }
+
+    // Método para atualizar o Slider da vida do castelo
+    private void UpdateCastleHealthUI()
+    {
+        castleHealthSlider.value = (float)gameManager.GetCastleHealth() / gameManager.GetMaxCastleHealth();
+    }
+
+    // Método para atualizar o texto de dinheiro do jogador usando TextMeshProUGUI
+    private void UpdateMoneyUI()
+    {
+        moneyText.text = gameManager.GetPlayerMoney().ToString() + "$";
+    }
+
+    // Método chamado quando o botão "QuitButton" é pressionado
+    public void OnQuitButtonPressed()
+    {
+        // Fecha o jogo
+        Application.Quit();
+    }
+
+    // Método chamado quando o botão "MainMenuButton" é pressionado
+    public void OnMainMenuButtonPressed()
+    {
+        // Volta para a cena principal (outra cena)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+
+    // Método chamado quando o botão "RestartButton" é pressionado
+    public void OnRestartButtonPressed()
+    {
+        // Reinicia o jogo (recarrega a cena atual)
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    // Método para ativar ou desativar o menu e o cursor
+    private void ToggleMenuAndCursor()
+    {
+        // Ativa ou desativa o GameObject do menu
+        menuObject.SetActive(!menuObject.activeSelf);
+
+        // Ativa ou desativa o cursor dependendo do estado do GameObject do menu
+        bool newState = menuObject.activeSelf;
+        SetMouseCursorState(newState);
+    }
+
+    // Método para definir o estado do cursor
+    private void SetMouseCursorState(bool newState)
+    {
+        Cursor.visible = newState;
+        Cursor.lockState = newState ? CursorLockMode.Confined : CursorLockMode.Locked;
+    }
+}
