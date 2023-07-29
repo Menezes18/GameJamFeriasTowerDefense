cb54140a6a14d105d42da40a4fd481da122bca7a
Gustavo Menezes <gustavo.e.menezes@outlook.com>
2023-07-29 05:54:43 -0300

    Animação de notificação

--- Assets/Scripts/GameManagerUI.cs
+++ Assets/Scripts/GameManagerUI.cs
@@ -7,22 +7,29 @@ public class GameManagerUI : MonoBehaviour
     public Slider castleHealthSlider;
     public TextMeshProUGUI moneyText;
     public TextMeshProUGUI waveText;
+    public TextMeshProUGUI pressTText;
     public GameObject menuObject;
 
     private GameManager gameManager;
     private WaveManager waveManager;
+    private bool isMenuActive = false;
 
+    
+    public string automaticoText = "Wave Automatico";
+    public string manualText = "Aperte T para iniciar a wave";
     void Start()
     {
         waveManager = FindObjectOfType<WaveManager>();
         gameManager = FindObjectOfType<GameManager>();
         UpdateUI();
+        UpdatePressTText();
     }
 
     void Update()
     {
         // Verifica constantemente a vida do castelo para atualizar o Slider
         UpdateUI();
+        UpdatePressTText();
 
         // Verifica se a tecla "P" foi pressionada
         if (Input.GetKeyDown(KeyCode.P))
@@ -79,19 +86,27 @@ public class GameManagerUI : MonoBehaviour
     // Método para ativar ou desativar o menu e o cursor
     private void ToggleMenuAndCursor()
     {
+        // Inverte o estado do menu
+        isMenuActive = !isMenuActive;
+
         // Ativa ou desativa o GameObject do menu
-        menuObject.SetActive(!menuObject.activeSelf);
+        menuObject.SetActive(isMenuActive);
 
         // Ativa ou desativa o cursor dependendo do estado do GameObject do menu
-        bool newState = menuObject.activeSelf;
+        bool newState = isMenuActive;
         SetMouseCursorState(newState);
+
+        // Pausa ou despausa o tempo dependendo do estado do menu
+        Time.timeScale = isMenuActive ? 0f : 1f;
     }
+
 
     // Método para definir o estado do cursor
     private void SetMouseCursorState(bool newState)
     {
         Cursor.visible = newState;
         Cursor.lockState = newState ? CursorLockMode.Confined : CursorLockMode.Locked;
+        
     }
 
 
@@ -103,6 +118,34 @@ public class GameManagerUI : MonoBehaviour
 
         // Oculta o cursor
         SetMouseCursorState(false);
-        Debug.Log("A");
+
+        // Despausa o tempo
+        Time.timeScale = 1f;
+    }
+
+    public void ButtonWaveAutomatic()
+    {
+        // Toggle the 'automatico' bool in the WaveManager class
+        waveManager.automatico = !waveManager.automatico;
+
+        // Update the visibility and text of the "Press T to start the next wave manually" text
+        UpdatePressTText();
+
+        // Optionally, you can display a message in the console to indicate the new state
+        Debug.Log("Wave Automatico: " + waveManager.automatico);
+    }
+
+    private void UpdatePressTText()
+    {
+        if (waveManager.automatico)
+        {
+            pressTText.text = automaticoText;
+        }
+        else
+        {
+            pressTText.text = manualText;
+        }
+
+        //pressTText.gameObject.SetActive(!waveManager.automatico);
     }
 }
